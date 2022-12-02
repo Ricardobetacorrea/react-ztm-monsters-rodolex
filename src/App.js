7f378276c4645224fb6e3e8be6ac97c8767bce8b
Fabian Ricardo Betancourt Correa <ricardobetacorrea@gmail.com>
2022-12-02 17:00:58 -0500

    Progreso hasta video 30

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      name: { firstName: 'Ricardo', lastName: 'Betancourt' },
+      company: 'OSC'
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hola {this.state.name.firstName } { this.state.name.lastName }, trabajas en {this.state.company} de momento</p>
+          <button onClick={() => { this.setState({ name: { firstName: 'Fabián Ricardo', lastName: 'Betancourt Correa' }}) }}>
+            Cambiar nombre
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
